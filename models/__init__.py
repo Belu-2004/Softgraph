a369085fe842dc60dc294f4abe64e1d99c73689d
santiidele22 <santiidele21@gmail.com>
2025-10-14 23:48:36 -0300

    agregue importaciones al __init__. comenzando

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .clientes import Cliente
+from .pedidos import Pedido
+from .presupuestos import Presupuesto
+
+__all__ = ["Cliente", "Presupuesto", "Pedido"]
--- services/gestor_clientes.py
+++ services/gestor_clientes.py
@@ -1,11 +1,13 @@
-from models.cliente import Cliente
-from services.persistencia import Persistencia
+from models.clientes import Cliente
+from services.persistencias import Persistencia
 
 RUTA_CLIENTES = "data/clientes.json"
 
+
 class GestorClientes:
     def __init__(self):
-        self.clientes = [Cliente.from_dict(c) for c in Persistencia.cargar_datos(RUTA_CLIENTES)]
+        self.clientes = [Cliente.from_dict(
+            c) for c in Persistencia.cargar_datos(RUTA_CLIENTES)]
 
     def agregar_cliente(self, nombre, tipo="ocasional"):
         cliente = Cliente(nombre, tipo)
@@ -26,4 +28,5 @@ class GestorClientes:
         return None
 
     def guardar(self):
-        Persistencia.guardar_datos(RUTA_CLIENTES, [c.to_dict() for c in self.clientes])
+        Persistencia.guardar_datos(
+            RUTA_CLIENTES, [c.to_dict() for c in self.clientes])
