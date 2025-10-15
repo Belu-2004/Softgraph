e7ad53669ba9ba822a338069c1fd5c99d0de1536
Belu-2004 <noebelu85@gmail.com>
2025-10-15 00:51:46 -0300

    modificacion

--- services/gestor_clientes.py
+++ services/gestor_clientes.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-from models.Clientes import Clientes
-from services.Persistencias import Persistencias
-=======
-from models.clientes import Cliente
-from services.persistencias import Persistencia
->>>>>>> santi
+from models import Cliente
+from services import Persistencia
 
 RUTA_CLIENTES = "data/clientes.json"
 
-
 class GestorClientes:
     def __init__(self):
-        self.clientes = [Cliente.from_dict(
-            c) for c in Persistencia.cargar_datos(RUTA_CLIENTES)]
+        self.clientes = [Cliente.from_dict(c) for c in Persistencia.cargar_datos(RUTA_CLIENTES)]
 
     def agregar_cliente(self, nombre, tipo="ocasional"):
         cliente = Cliente(nombre, tipo)
@@ -33,5 +26,4 @@ class GestorClientes:
         return None
 
     def guardar(self):
-        Persistencia.guardar_datos(
-            RUTA_CLIENTES, [c.to_dict() for c in self.clientes])
+        Persistencia.guardar_datos(RUTA_CLIENTES, [c.to_dict() for c in self.clientes])
